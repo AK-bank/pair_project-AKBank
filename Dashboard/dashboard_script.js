23c086da391ed65f745481f81a345de605f6a4d2
khalilbenali1 <khalil.ben.ali16121@gmail.com>
2022-08-23 14:04:04 +0100

    functions almost done

--- Dashboard/dashboard_script.js
+++ Dashboard/dashboard_script.js
@@ -5,14 +5,20 @@ function Client(init,firstName,lastName,email,password){
 		lastName:lastName,
 		email:email,
 		password:password,
-		balance:init,
+		balance:Number(init),
 		withdraw: withdrawFunction, 
 		deposit: depositFunction,
     transfer: trasferFunction
 		}
 }
+
+
+
 function IDGenerator(){
-	var id=0
+
+	var id=0;
+	localStorage.setItem("counter",JSON.stringify(id));
+	id=JSON.parse(localStorage.getItem(id));
 	function generator(){
 		id++;
 		return id
@@ -22,21 +28,52 @@ function IDGenerator(){
 var ids=IDGenerator()
 
 var trasferFunction=function (uniqueID,money){
-     	this.balance=this.balance-money;
-     	uniqueID.deposit(money)
-     }
+	
+		if (money>this.balance)
+			{
+				return "withdrawal Denied"
+			}
+    if (money<=this.balance){
+    	console.log("te5dem")
+      this.balance-=money;
+      for(var i=0;i<clients.length;i++){
+      	
+      	if(parseInt(clients[i].id)===parseInt(uniqueID)){
+      		
+      		clients[i].deposit(money);
+      		return;
+      	}
+      }
+}
+}
+    
+     
 var depositFunction= function(money){
-       this.balance=this.balance+money
-       return "your balance is: "+this.balance;
+	console.log(typeof this.balance,typeof money, )
+       this.balance+=money;
+       this.balance;
+       console.log(this.balance)
+       return this.balance;
+
      }       
 
 var withdrawFunction=function(money){if (money>this.balance)
 	{return "withdrawal Denied"}
-         if (money<=this.balance){this.balance=this.balance-money
-          return "here's your money: "+money}
+         if (money<=this.balance){var result=this.balance-=money
+         
+         return this.balance}
       }
 
 var clients=[];
+clients=JSON.parse(localStorage.getItem('client'));
+for(var i=0;i<clients.length;i++){
+	clients[i].withdraw=withdrawFunction;
+	clients[i].deposit=depositFunction;
+	clients[i].transfer=trasferFunction
+
+}
+
+JSON.parse(localStorage.getItem('client'));
 
 function strongPasswords(string){ 
       var checkUpper=/[A-Z]/;
@@ -55,25 +92,60 @@ function strongPasswords(string){
 $("#signUp").click(function(){
 	if(strongPasswords($("#signUpPass").val())){
  		var k=Client($("#Balance").val(),$("#firstName").val(),$("#lastName").val(),$("#signUpEmail").val(),$("#signUpPass").val())
+		JSON.parse(localStorage.getItem('client'));
 		clients.push(k);
+		localStorage.setItem('client', JSON.stringify(clients));
+		
+
  	}
  	else{
  		$("#weakPass").append("<p>the Password should have :<ul><li>minimum 8 length</li><li>contains a number</li><li>contains an upperCase and lowerCase caracter</li><li>contains a symbol</li></ul> </p>")
  	}
-		
 })
 
-
-
+var connectedClient;
+connectedClient=JSON.parse(localStorage.getItem('connected'));
  $("#signIn").click(function(){
  	for(var i=0;i<clients.length;i++){
  		if(clients[i].email===$('#signInemail').val() && clients[i].password===$('#signInpass').val()){
- 			console.log('5edmet')
- 			return true;
+ 			 connectedClient=clients[i]
+ 			 localStorage.setItem('connected', JSON.stringify(connectedClient));
+				window.open("../dashboard/dashboard.html");
+ 			 return ;
  		}
  	}
  	$("#wrong").append("<p>Wrong E-mail or Password</p>");
  	
  })
-      
+ connectedClient.withdraw=withdrawFunction;
+	connectedClient.deposit=depositFunction; 
+	connectedClient.transfer=trasferFunction;
+function openMethod(evt,method ) {
+  var i, x, tablinks;
+  x = $(".behaviour");
+  for (i = 0; i < x.length; i++) {
+    x.eq(i).css("display","none");
+  }
+  tablinks = $(".tablink");
+  for (i = 0; i < x.length; i++) {
+    
+  }
+  $('#'+method).css("display","block");
+  
+}
 
+$(".welcome").text("welcome " + connectedClient.firstName +" "+ connectedClient.id);
+$("#balanceDisplay").text("Your Balance is : "+connectedClient.balance);
+$("#withdraw_button").click(function(){
+	connectedClient.withdraw(parseInt($("#withdrawAmount").val()))
+});
+
+	
+$("#deposit_button").click(function(){
+
+connectedClient.deposit(parseInt($("#depositAmount").val()))
+}); 			 
+
+$('#transfer_button').click(function(){
+	connectedClient.transfer(parseInt($('#clientID').val()),parseInt($('#transfeAmount').val()))
+})
